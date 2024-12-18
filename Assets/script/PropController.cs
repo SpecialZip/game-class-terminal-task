77d95fa9d18638cef17009340160afc030db706e
SpecialZip <2949916908@qq.com>
2024-12-18 18:15:07 +0800

    增加了道具盒子

---
+++ Assets/script/PropController.cs
@@ -0,0 +1,28 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PropController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    
+    private void OnTriggerEnter(Collider other)
+    {
+        Vector3 position =gameObject.transform.position;
+        Quaternion rotation = other.gameObject.transform.rotation;
+        Debug.Log(position);Debug.Log(rotation);
+        GameObject.Find("Props").GetComponent<PropScriptRespawn>().Respawn(position, rotation);
+        Destroy(gameObject);
+    }
+}
